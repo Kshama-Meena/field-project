0cc70e16536bcb5029ad80a1f89d1ae646f8f3f9
Kshama-Meena <kshamameena534@gmail.com>
2025-09-25 15:14:51 +0530

    home comment remove

--- project/src/componet/Home.jsx
+++ project/src/componet/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="pt-28">  {/* 👈 Navbar ke niche space dene ke liye */}
+    <div className="pt-28">  
       <h1 className="text-4xl font-bold">WELL COME</h1>
     </div>
   );
